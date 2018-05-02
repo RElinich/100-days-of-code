e6cdad378f8d54ca61ff6e4b269cc39b4532c2d5
Becky <Becky@10.0.0.92>
2018-05-02 17:04:30 -0400

    Changed Layout and further integration of W3 CSS practices for skeleton

---
+++ w3cssWebsite/public_html/js/dateDisplay.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Purpose of this is to provide a date display that will be linked to in the webpage
+
+//To hold the Date:
+var theDate = new Date();
+
+//Displaying the date:
+document.getElementById('dateContainer').innerHTML = theDate;
+
